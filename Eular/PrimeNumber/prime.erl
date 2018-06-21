acdf592be4be2e19431ec0276b19a95c404be17e
pavan_kumar <paone.avu@gmail.com>
2018-06-21 11:57:05 +0530

    Prime Number finding Sample

--- Eular/PrimeNumber/prime.erl
+++ Eular/PrimeNumber/prime.erl
@@ -3,11 +3,17 @@
 
 isPrime(N, I) when I < N/2 -> 
     io:format("::::: ~w iteration ~n", [I]),
+    Temp = N/2,
     if 
-        N rem I =/= 0 -> io:format(":::::~w rem ~w ~n", [N, I]), isPrime(N, I+1);
+        N rem I =/= 0 -> io:format(":::::~w rem ~w Temp::~w~n", [N, I, Temp]),
+            if
+                (I + 1) > Temp -> 1;
+                true -> isPrime(N, I + 1) end;
         true -> isPrime(0, 0)
     end;
-isPrime(_, _) -> io:format("Entered Number is not a prime number...~n"), 0.
+isPrime(2, 2) -> io:format("Entered Number is a prime number...~n"), 1;
+isPrime(1, 1) -> io:format("Entered Number is a prime number...~n"), 1;
+isPrime(0, 0) -> io:format("Entered Number is not a prime number...~n"), 0.
 
 main(N) ->
     X = isPrime(N, 2),
