be037e459dab5671df54d06f8e00f8836ee711c1
System Administrator <root@SportsIT-Air.routerd522cc.com>
2018-06-25 14:06:16 +0530

    Prime highest factor solution

--- Eular/HighestPrimeFactor/prime_factor.erl
+++ Eular/HighestPrimeFactor/prime_factor.erl
@@ -1,50 +1,60 @@
 % The prime factors of 13195 are 5, 7, 13 and 29.
 
 % What is the largest prime factor of the number 600851475143 ?
-
+%       Ans:6857
 
 -module(prime_factor).
--export([findPrimeFactors/3, main/1]).
+-export([isPrime/2, findFactors/2, findHighestPrimeFactorsInList/1, main/1]).
 
 %%%%%   %%%%%   %%%%%   %%%%%   %%%%%  To check Prime  %%%%%   %%%%%   %%%%%   %%%%%   %%%%%
-isPrime(N, I) when I < N/2 -> 
-    Temp = N div I,
-    io:format("::::: ~w iteration N::~w  Temp::~w ~n", [I, N, Temp]),
-    if 
-        N rem I =/= 0 -> io:format(":::::~w rem ~w Temp::~w~n", [N, I, Temp]),
-            if
-                (I + 1) > Temp -> 1;
-                true -> isPrime(N, I + 1) end;
-        true -> isPrime(0, 0)
-    end;
-isPrime(3, 2) -> io:format("Entered Number is a prime number...~n"), 1;
-isPrime(2, 2) -> io:format("Entered Number is a prime number...~n"), 1;
-isPrime(1, 1) -> io:format("Entered Number is a prime number...~n"), 1;
-isPrime(0, 0) -> io:format("Entered Number is not a prime number...~n"), 0.
-%%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%
-
-%%%%%   %%%%%   %%%%%   %%%%%   %%%%%   To find Factors   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%
-findPrimeFactors(Num, Q, I) when I < (Q / 2) ->
-io:format("Num:::~w Q:::~w I is:~w ~n", [Num, Q, I]),
-Remainder = Q rem I,
+isPrime(Num, Num) when Num > 0 -> 1;
+isPrime(0, 0) -> 0;
+isPrime(N, I) ->        %when I =< (N div 2)
+    Quotient = N div 2,
     if
-        Remainder =:= 0 ->
-            Devidend = Q div I,
-            IsPrimeNumber = isPrime(Devidend, 2),
-            io:format("Dividend:~w:::::isPrime::~w ~n", [Devidend, IsPrimeNumber]),
-            if 
-                IsPrimeNumber =:= 1 -> [Devidend];
+        I =< Quotient ->
+            if
+                N rem I =/= 0 ->
+                    if
+                        (I + 1) >= (N div 2) -> isPrime(1, 1);
+                        true -> isPrime(N, I+1) end;
+                true -> isPrime(0, 0) end;
+        true -> isPrime(1, 1) end.
+%%%%%   %%%%%   %%%%%   %%%%%   %%%%%   To find Factors   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%
+findFactors(Num, I) ->          %when I < (Num / 2)
+    Quotient = Num div 2,
+    io:format("Num::~w, I::~w Q::~w ~n", [Num, I, Quotient]),
+    if
+        I =< Quotient ->
+            if
+                Num rem I =:= 0 -> 
+                    [I | findFactors(Num div I, 2)];
                 true -> 
-                    io:format("rem:::~w  I is:~w:::Q::~w ~n", [Remainder, I, Q]),
-                    [I | findPrimeFactors(Num, Devidend, 2)] end;
-        true -> 
-            io:format("rem:::~w  I :~w is not a factor... ~n", [Num rem I, I]),
-            [1 | findPrimeFactors(Num, Q, I + 1)] %findPrimeFactors(Num, I + 1)
-     end;
-findPrimeFactors(_, _, _) -> io:format("~n ...Empty... ~n"),[].
+                    findFactors(Num, I + 1) end; %[1 | findFactors(Num, I + 1)] end;
+        true -> [Num] end.
+
+%%%%%   %%%%%   %%%%%   %%%%%   %%%%%   To find Prime Factors in a list   %%%%%   %%%%%   %%%%%   %%%%%   %%%%% 
+
+findHighestPrimeFactorsInList([H | T]) -> 
+    IsPrime = isPrime(H, 2),
+    if
+        IsPrime =:= 1 -> H;
+        true -> findHighestPrimeFactorsInList(T) end.
+
 %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%   %%%%%
 main(N) ->
-    X = findPrimeFactors(N, N, 2),
-    Y = lists:usort(X),
-    Z = lists:delete(1, Y),
-    io:format("Prime Factors:~w...~n Y:~w ~nZ:~w", [X, Y, Z]).
+    X = findFactors(N, 2),
+    ReverseSortedList = lists:reverse(lists:sort(X)),
+    PHF = findHighestPrimeFactorsInList(ReverseSortedList),
+    io:format("Factors:~w...~n PrimeHighestFactor:~w ~n", [X, PHF]).
+
+
+
+
+
+
+    % io:format("Factors:~w...~n PHF:~w ~n", [X, PHF]),
+    % io:format("SortedList:~w...~n", [ReverseSortedList]),
+    % Y = lists:usort(X),
+    % Z = lists:delete(1, Y),
+    % io:format("Prime Factors:~w...~n Y:~w ~nZ:~w", [X, Y, Z]).
--- Others/factors.erl
+++ Others/factors.erl
@@ -1,17 +1,34 @@
 -module(factors).
--export([findFactors/2, main/1]).
+-export([findFactors/2, main/1, num/2]).
 
-findFactors(Num, I) when I < (Num / 2) ->
+num([H|T], Count) -> num(T, Count + 1);
+num([], Count) -> Count.
+
+findFactors(Num, I) ->          %when I < (Num / 2)
+    Quotient = Num div 2,
     if
-        Num rem I =:= 0 -> 
-            [I | findFactors(Num, I + 1)];
-        true -> 
-            [1 | findFactors(Num, I + 1)]
-     end;
-findFactors(_, _) -> [].
+        I =< Quotient ->
+            if
+                Num rem I =:= 0 -> 
+                    [I | findFactors(Num, I + 1)];
+                true -> 
+                    findFactors(Num, I + 1) end; %[1 | findFactors(Num, I + 1)] end;
+        true -> [] end.
+% findFactors(_, _) -> io:format("Empty Values"), [].
 
 main(N) ->
     X = findFactors(N, 2),
-    Y = lists:usort(X),
-    Z = lists:delete(1, Y),
-    io:format("Factors:~w...~n Y:~w ~nZ:~w", [X, Y, Z]).
+    Length = num(X, 0),
+    if
+        Length =:= 0 -> io:format("Entered Number ~w is a prime number and the only factor is :~w. ~n", [N, N]);
+        true -> io:format("Factors:~w ~n", [X]) end.
+        
+
+
+
+
+
+    % io:format("Factors:~w...Length::~w.~n", [X, Length]).
+    % Y = lists:usort(X),
+    % Z = lists:delete(1, Y),
+    % io:format("Factors:~w...~n Y:~w ~nZ:~w", [X, Y, Z]).
